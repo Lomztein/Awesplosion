31e31c9b58cfcc379067fc31c1753e90e56267f0
Lomztein <lomztein2@gmail.com>
2013-12-03 23:35:34 +0100

    Changed stuff and stuff
    I can't quite remember

--- Assets/ParticleScript.cs
+++ Assets/ParticleScript.cs
@@ -3,8 +3,15 @@ using System.Collections;
 
 public class ParticleScript : MonoBehaviour {
 
+	public GameObject light;
+
 	// Use this for initialization
 	void Start () {
 		Destroy(gameObject,1);
+	}
+	void Update() {
+		if (light) {
+			light.light.intensity -= 3 * Time.deltaTime;
+		}
 	}
 }
