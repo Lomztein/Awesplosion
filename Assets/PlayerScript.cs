384098123689c7d26d9163712ab9d2d56bced2f7
Lomztein <lomztein2@gmail.com>
2013-12-03 13:29:47 +0100

    Initial Commit
    Beginning of Awesplosion, a sorta 2.5D platformer based on explosions.

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float directionToMouse = Vector2.Angle(transform.position,Camera.main.ScreenToViewportPoint(Input.mousePosition));
+		Debug.Log (directionToMouse);
+	}
+}
